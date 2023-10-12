e8a54349c50f49d45db8aac872343c2bc5d3497a
AnnBelova <anutik-89@tut.by>
2023-10-12 23:41:17 +0300

    задание к контрольной- программа

---
+++ zadacha1/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic;
+
+int Input(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+string[] CreateArray(int size)
+{
+    return new string[size];
+}
+void FillArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine("Введите строку: ");
+        arr[i] = Console.ReadLine()!;
+    }
+}
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        Console.Write(arr[i] + " ");
+    }
+
+    Console.WriteLine();
+}
+string[] NewArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+            count++;
+    }
+    string[] res = CreateArray(arr.Length);
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            res[j] = arr[i];
+            j++;
+        }
+    }
+    return res;
+}
+
+int size = Input("Введите размер массива: ");
+string[] myArray = CreateArray(size);
+FillArray(myArray);
+System.Console.WriteLine("Введенный массив: ");
+PrintArray(myArray);
+string[] newarr = NewArray(myArray);
+System.Console.WriteLine("Новый массив: ");
+PrintArray(newarr);
